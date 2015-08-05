22f8cdb3409db4be63fa59909296ed781f75f1e4
Kamil Szymanski <kamil.szymanski.dev@gmail.com>
2015-08-05 06:56:42 +0200

    add javadocs

--- src/main/java/pl/ppl/common/infrastructure/repository/FetchableJpaRepository.java
+++ src/main/java/pl/ppl/common/infrastructure/repository/FetchableJpaRepository.java
@@ -3,10 +3,20 @@ package pl.ppl.common.infrastructure.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import javax.persistence.EntityNotFoundException;
 import java.io.Serializable;
 
 @NoRepositoryBean
 public interface FetchableJpaRepository<T, ID extends Serializable> extends JpaRepository<T, ID> {
 
+    /**
+     * Retrieves an entity by its id or thorws EntityNotFoundException if such entity does not exists.
+     *
+     * Please bare in mind that Spring Framework automatically translates EntityNotFoundException into JpaObjectRetrievalFailureException.
+     *
+     * @param id must not be {@literal null}.
+     * @return the entity with the given id
+     * @throws EntityNotFoundException if entity with the given id does not exists
+     */
     T fetchById(ID id);
 }
