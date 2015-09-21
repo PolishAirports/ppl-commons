5c0e94a9165f9984f0016ba7cd664ca5c3254915
Kamil Szymanski <kamil.szymanski.dev@gmail.com>
2015-09-21 23:32:14 +0200

    [#2] add javadocs

--- src/main/java/pl/ppl/common/config/EnableCustomSpringDataJpaRepositories.java
+++ src/main/java/pl/ppl/common/config/EnableCustomSpringDataJpaRepositories.java
@@ -1,6 +1,7 @@
 package pl.ppl.common.config;
 
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import pl.ppl.common.infrastructure.repository.FetchableJpaRepository;
 import pl.ppl.common.infrastructure.repository.FetchableJpaRepositoryFactoryBean;
 
 import java.lang.annotation.ElementType;
@@ -8,6 +9,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Enables Spring Data support for {@link FetchableJpaRepository}
+ *
+ * This annotation is intended to be put on configuration classes.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @EnableJpaRepositories(repositoryFactoryBeanClass = FetchableJpaRepositoryFactoryBean.class)
--- src/main/java/pl/ppl/common/infrastructure/repository/FetchableJpaRepository.java
+++ src/main/java/pl/ppl/common/infrastructure/repository/FetchableJpaRepository.java
@@ -2,10 +2,17 @@ package pl.ppl.common.infrastructure.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import pl.ppl.common.config.EnableCustomSpringDataJpaRepositories;
 
 import javax.persistence.EntityNotFoundException;
 import java.io.Serializable;
 
+/**
+ * Enriches Spring Data {@link JpaRepository} with ability to fetch an entity by id and fail if such entity does not exists in the database.
+ *
+ * To use this repository you have to make Spring Data infrastructure aware of {@code FetchableJpaRepositoryImpl}.
+ * You can do that by putting {@link EnableCustomSpringDataJpaRepositories} annotation on your configuration class.
+ */
 @NoRepositoryBean
 public interface FetchableJpaRepository<T, ID extends Serializable> extends JpaRepository<T, ID> {
 
