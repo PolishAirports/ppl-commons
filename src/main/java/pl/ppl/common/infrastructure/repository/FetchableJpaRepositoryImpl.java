70cbe61a44ee54eb4cafe00ecb54e7e4d093e220
Niczyporuk, Michał <M.Niczyporuk@polish-airports.com>
2016-03-03 17:15:53 +0100

    add jpa specification and entity graph support

--- src/main/java/pl/ppl/common/infrastructure/repository/FetchableJpaRepositoryImpl.java
+++ src/main/java/pl/ppl/common/infrastructure/repository/FetchableJpaRepositoryImpl.java
@@ -1,17 +1,27 @@
 package pl.ppl.common.infrastructure.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.TypedQuery;
 import java.io.Serializable;
+import java.util.List;
 
 public class FetchableJpaRepositoryImpl<T, ID extends Serializable>
         extends SimpleJpaRepository<T, ID>
         implements FetchableJpaRepository<T, ID> {
 
+    private EntityManager em;
+
     public FetchableJpaRepositoryImpl(Class<T> domainClass, EntityManager entityManager) {
         super(domainClass, entityManager);
+        this.em = entityManager;
     }
 
     @Override
@@ -22,5 +32,33 @@ public class FetchableJpaRepositoryImpl<T, ID extends Serializable>
             throw new EntityNotFoundException(msg);
         }
         return entity;
+    }
+
+    @Override
+    public List<T> findAll(Specification<T> spec, EntityGraph.EntityGraphType entityGraphType, String entityGraphName) {
+        TypedQuery<T> query = getQuery(spec, (Sort) null);
+        query.setHint(entityGraphType.getKey(), em.getEntityGraph(entityGraphName));
+        return query.getResultList();
+    }
+
+    @Override
+    public Page<T> findAll(Specification<T> spec, Pageable pageable, EntityGraph.EntityGraphType entityGraphType, String entityGraphName) {
+        TypedQuery<T> query = getQuery(spec, pageable.getSort());
+        query.setHint(entityGraphType.getKey(), em.getEntityGraph(entityGraphName));
+        return readPage(query, pageable, spec);
+    }
+
+    @Override
+    public List<T> findAll(Specification<T> spec, Sort sort, EntityGraph.EntityGraphType entityGraphType, String entityGraphName) {
+        TypedQuery<T> query = getQuery(spec, sort);
+        query.setHint(entityGraphType.getKey(), em.getEntityGraph(entityGraphName));
+        return query.getResultList();
+    }
+
+    @Override
+    public T findOne(Specification<T> spec, EntityGraph.EntityGraphType entityGraphType, String entityGraphName) {
+        TypedQuery<T> query = getQuery(spec, (Sort) null);
+        query.setHint(entityGraphType.getKey(), em.getEntityGraph(entityGraphName));
+        return query.getSingleResult();
     }
 }
